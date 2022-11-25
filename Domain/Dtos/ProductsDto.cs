74e36096c84ef6d0f237f8b2aae8c6d4299d87f8
MuhammadMumtoz <nasim23@mail.ru>
2022-11-25 18:54:57 +0500

    first commit

---
+++ Domain/Dtos/ProductsDto.cs
@@ -0,0 +1,10 @@
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public decimal Price { get; set; }
+    public int Amount { get; set; }
+    public int Stockcode { get; set; }
+
+}
